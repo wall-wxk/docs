519e6c28058478a056d6031e0c68ede0be8ccb5f
leon <582104384@qq.com>
2021-07-06 12:30:12 +0800

    feat: 增加百度统计

---
+++ _assets/_js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e1954f0b9e886bd02d54ea31a40c4aba";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
