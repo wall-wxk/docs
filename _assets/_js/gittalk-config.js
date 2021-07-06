f11a02c162f6037b1161b63ecc87abb075b76ca8
leon <582104384@qq.com>
2021-07-06 12:01:44 +0800

    fix: 修复gittalk问题

--- _assets/_js/gittalk-config.js
+++ _assets/_js/gittalk-config.js
@@ -6,10 +6,10 @@ var loadGittalk = function () {
   }
   var gitalk = new Gitalk({
     clientID: "15fdb6465cf7c7fe6342",
-    clientSecret: " db93e28da52497861e9bdfe4280cfda98c8840fa",
+    clientSecret: "15f4518ec1d1dc163f427f2d5d3ac4266a5ff452",
     repo: "interview",
     owner: "wall-wxk",
-    admin: ["请登录GitHub后进行评论"],
+    admin: ["wall-wxk"],
     id: location.hash, // Ensure uniqueness and length less than 50
     distractionFreeMode: false, // Facebook-like distraction free mode
   });
